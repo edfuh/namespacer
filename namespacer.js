7805a0607883289c8974bd369ee04dc782a37f98
Ed Fuh <ed@storenvy.com>
2012-03-28 12:14:24 -0700

    You know when you read something you wrote a long time ago and are like "wtf was i thinking" ...yeah

--- namespacer.js
+++ namespacer.js
@@ -2,41 +2,34 @@
   * Namespacer: you know, for namespacing
   * Ed Fuhrken
   * https://github.com/edfuh/namespacer
-  * License MIT
+  * License WTFPL
   */
-(function (name, definition) {
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = definition();
-    } else if (typeof define === 'function') {
-        define(definition);
-    } else {
-        this[name] = this['ns'] = definition();
-    }
-}('namespacer', function () {
-    function namespacer(ns, obj) {
-        // namespace name must be a non empty string
-        if (typeof ns !== "string" || !ns.length) {
-            throw new Error('[namespacer] Invalid namespace');
-        }
-
-        var parts = ns.split('.'),
-            namespace = this,
-            i = 0,
-            j = parts.length,
-            b;
-
-        for (; i < j; i++) {
-            namespace = namespace[parts[i]] = namespace[parts[i]] || {};
-        }
-
-        if (typeof obj === 'object') {
-            for (b in obj) {
-                namespace[b] = obj[b];
-            }
-        }
-
-        return namespace;
+!function () {
+  function namespacer(ns, obj) {
+    // namespace name must be a non empty string
+    if (typeof ns !== "string" || !ns.length) {
+      throw new Error('[namespacer] Invalid namespace');
     }
 
-    return namespacer;
-}));
+    var 
+      parts = ns.split('.'),
+      namespace = this,
+      i = 0,
+      j = parts.length,
+      b;
+
+    for (; i < j; i++) {
+      namespace = namespace[parts[i]] = namespace[parts[i]] || {};
+    }
+
+    if (typeof obj === 'object') {
+      for (b in obj) {
+        namespace[b] = obj[b];
+      }
+    }
+
+    return namespace;
+  }
+
+  this['namespacer'] = this['ns'] = namespacer;
+}();
--- tests/node-test.js
+++
@@ -1,5 +0,0 @@
-var assert = require('assert'),
-    ns = require('../namespacer.js');
-
-ns('EDFUH.is.a.baller');
-assert.ok(typeof EDFUH.is.a.baller === 'object');
